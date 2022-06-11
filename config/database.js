4252dafa004b85b4e366cd401deb4f7ba3cf9a47
suvasis999 <talk2suvasis@gmail.com>
2022-06-11 19:19:05 +0530

    'updates'

--- config/database.js
+++ config/database.js
@@ -1,13 +1,14 @@
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'db_memorial'),
-      user: env('DATABASE_USERNAME', 'db_user'),
-      password: env('DATABASE_PASSWORD', 'pass@1234'),
-      ssl: env.bool('DATABASE_SSL', false),
+        client: 'postgres',
+        connection: {
+            host: env('DATABASE_HOST', '127.0.0.1'),
+            port: env.int('DATABASE_PORT', 5432),
+            database: env('DATABASE_NAME', 'db_memorial'),
+            user: env('DATABASE_USERNAME', 'db_user'),
+            password: env('DATABASE_PASSWORD', 'pass@1234'),
+            ssl: env.bool('DATABASE_SSL', false),
+
+        },
     },
-  },
 });
